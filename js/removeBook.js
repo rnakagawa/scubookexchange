fa32e9ef17170f8b35eb74d930aac9ae4def5511
Ryan Nakagawa <rnakagawa@scu.edu>
2015-12-01 11:54:43 -0800

    remove book page added and remove book script added, nearly done

---
+++ js/removeBook.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  var email = prompt("Please enter your SCU email");
+  var verification = prompt("Please enter your verfication code");
+  var info = {
+    email: email,
+    verfication: verification
+  };
+
+  $.ajax({
+    type: 'GET',
+    url: 'php/removeBook.php',
+    data: {'info': info}
+  }).success(function(response){
+    if(response==='success'){
+      alert('Your book has been removed!');
+      window.location.href = "index.html";
+    }
+    else{
+      alert('Error, the email or verification code does not match. Please try again.');
+    }
+  });
+});
---
+++ php/removeBook.php
@@ -0,0 +1,35 @@
+<?php
+  $check = GET['info'];
+  $ver = $check['verification'];
+
+  if(filter_var($check['email'], FILTER_VALIDATE_EMAIL))
+		$email=$check['email'];
+	else{
+		echo 'Invalid email';
+	}
+  if (condition) {
+    # code...
+  }
+
+  $dbhost="dbserver.engr.scu.edu:3306";
+	$dbuser="lwong";
+	$dbpass="mysqldb";
+	$dbname="sdb_lwong";
+	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die('Error connecting to db');
+
+	$query='SELECT * FROM books WHERE vCode = "'.$ver.'" AND email = "'.$email.'"';
+	$result=mysqli_query($conn, $query);
+	if($result == null){
+		echo('false');
+	}
+  else {
+    //$row=$result->fetch_assoc();
+    $stmt = $conn->prepare("DELETE FROM books WHERE vCode = ? AND email = ?");
+    $stmt->bind_param('ss', $ver, $email);
+    $stmt->execute();
+    $stmt->close();
+		//mysqli_query($conn, $stmt);
+    echo 'success';
+  }
+	$conn->close();
+?>
