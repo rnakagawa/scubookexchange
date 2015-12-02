1c913ff9650b098ef810916b1f746f15eadc7ec5
Leland <lelandr.wong@gmail.com>
2015-12-01 16:12:47 -0800

    retrying gitignore

--- php/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$dbhost="dbserver.engr.scu.edu:3306";
-	$dbuser="lwong";
-	$dbpass="mysqldb";
-	$dbname="sdb_lwong";
-	$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die('Error connecting to db');
-?>
